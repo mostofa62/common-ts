7176bad5a9098f694b3da45db099daf37de5f890
Golam Mostofa <golammostofa31188@gmail.com>
2023-01-11 17:07:49 +0600

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -4,7 +4,7 @@ export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    version:number;
+    version: number;
     ticket: {
       id: string;
     };
